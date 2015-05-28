05feefcb0059bee8f826c9efd266a9bf4c7fdae2
Mike <mike@marketyze.com>
2015-05-28 18:14:06 +0300

    testing Angular

--- test/js/DynamicTableAngular.js
+++ test/js/DynamicTableAngular.js
@@ -1,7 +1,12 @@
 ﻿(function () {
     var app = angular.module("DynamicGrid", []);
     var MainController = function ($scope, $http) {
+        $scope.imgSrc = 'images/plus.jpg';
+
         var onGetParrentsComplete = function (response) {
+            $scope.accTypes = response.data;
+        }
+        var onGetChildrenComplete = function (response) {
             $scope.accounts = response.data;
         }
         var onError = function (reason) {
@@ -9,7 +14,13 @@
         };
         
         $http.get("GetNestedGridData.ashx?rt=GetParrents")
-        .then(onGetParrentsComplete,onError);
+        .then(onGetParrentsComplete, onError);
+
+        $scope.GetFirstLevel = function (parrentId) {
+            $scope.imgSrc = 'images/minus.jpg';
+            $http.get("GetNestedGridData.ashx?rt=GetChildren&ParrentId=" + parrentId)
+            .then(onGetChildrenComplete, onError);
+        };
     };
 
     app.controller("MainController", ["$scope", "$http", MainController]);
